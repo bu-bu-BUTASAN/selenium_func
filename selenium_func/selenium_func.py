11b34f428d02b9d00f1c7f27aa6d28517f880470
butasan <tomoyuki.u330@gmail.com>
2022-05-14 18:51:14 +0900

    a

--- selenium_func/selenium_func.py
+++ selenium_func/selenium_func.py
@@ -3,12 +3,12 @@ from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.keys import Keys
 
 ### seleniumでのクリック関数を定義
-def web_ck(driver,a_xpath,t_sleeptime) :
+def ck(driver,a_xpath,t_sleeptime) :
     driver.find_element_by_xpath(a_xpath).click()
     time.sleep(int(t_sleeptime))
 
 ### seleniumでのリスト選択関数を定義
-def web_listselect(driver,a_xpath,b_value,t_sleeptime) :
+def listselect(driver,a_xpath,b_value,t_sleeptime) :
     dropdown = driver.find_element_by_xpath(a_xpath)
     select = Select(dropdown)
     try :
@@ -18,18 +18,18 @@ def web_listselect(driver,a_xpath,b_value,t_sleeptime) :
     time.sleep(int(t_sleeptime))
 
 ### seleniumでの入力関数を定義
-def web_nyuryoku(driver,a_xpath,b_naiyou,t_sleeptime) :
+def nyuryoku(driver,a_xpath,b_naiyou,t_sleeptime) :
     texts_title = driver.find_element_by_xpath(a_xpath)
     texts_title.send_keys(b_naiyou)
     time.sleep(int(t_sleeptime))
 
 ### seleniumでのセレクト関数を定義
-def web_select(driver,a_xpath,t_sleeptime) :
+def select(driver,a_xpath,t_sleeptime) :
     driver.find_element_by_xpath(a_xpath).select
     time.sleep(int(t_sleeptime))
 
 ### seleniumでのクリア関数を定義
-def web_clear(driver,a_xpath,t_sleeptime) :
+def clear(driver,a_xpath,t_sleeptime) :
     driver.find_element_by_xpath(a_xpath).send_keys((Keys.CONTROL + "a"))
     driver.find_element_by_xpath(a_xpath).send_keys(Keys.DELETE)
     time.sleep(int(t_sleeptime))
